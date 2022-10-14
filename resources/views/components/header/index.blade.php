c41bc47ef726a98d01bbd76ef3d7ee1f01fcf7d0
esteban-florez <eflorez077@gmail.com>
2022-10-14 11:48:55 -0400

    revamped header

---
+++ resources/views/components/header/index.blade.php
@@ -0,0 +1,29 @@
+<header class="main-header navbar navbar-expand navbar-white navbar-light">
+  <ul class="navbar-nav align-items-center">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+        <i class="fas fa-bars"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <span class="d-none d-sm-inline-block h5 m-0">Departamento de Vinculación Social</span>
+    </li>
+  </ul>
+  <img src="{{ asset('img/logo-upta.png') }}" alt="Logo de la UPTA" class="brand-image footer-img d-sm-none"
+    height="35">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item dropdown">
+      <a class="nav-link" data-toggle="dropdown" href="#">
+        <i class="far fa-bell navbar-icon"></i>
+        <span class="badge badge-danger navbar-badge">2</span>
+      </a>
+      <x-header.dropdown/>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" role="button" data-toggle="modal" data-target="#infoModal">
+        <i class="fas fa-info-circle mr-1"></i>
+        <span class="d-none d-md-inline">Contáctanos</span>
+      </a>
+    </li>
+  </ul>
+</header>
---
+++ resources/views/components/header/notf-card.blade.php
@@ -0,0 +1,17 @@
+<a href="{{ $url }}" class="dropdown-item">
+  <div class="media">
+    <div class="media-body">
+      <h3 class="dropdown-item-title">
+        {{ $title }}
+        <span class="float-right text-sm">
+          <i class="fas fa-times"></i>
+        </span>
+      </h3>
+      <p class="text-sm">{{ $subtitle }}</p>
+      <p class="text-sm text-muted">
+        <i class="far fa-clock mr-1">
+        </i> Hace 5 horas // TODO: make timestamp work
+      </p>
+    </div>
+  </div>
+</a>
