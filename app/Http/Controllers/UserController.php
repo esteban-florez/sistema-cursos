1e94afcb90be828effdca38bd2a4b8102ae77202
Myriam <myaqueno@gmail.com>
2022-10-24 00:27:40 -0400

    validación del registro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,8 +14,23 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
+        $user = $request->validate([
+            'password' => ['required', 'confirmed', 'min:8', 'max:20'],
+            'first_name' => ['required', 'max:25'],
+            'second_name' => ['max:20'],
+            'first_lastname' => ['required', 'max:25'],
+            'second_lastname' => ['max:20'],
+            'ci' => ['required', 'max:25'],
+            'ci_type' => ['required'],
+            'email' => ['required', 'email', 'max:50', 'unique:users,email'],
+            'birth' => ['required', 'date'],
+            'gender' => ['required'],
+            'phone' => ['required', 'max:25'],
+            'grade' => ['required'],
+            'address' => ['required', 'string', 'max:150'],
+        ]);
+        
         $user = User::create([
-            'username' => $request->username,
             'password' => bcrypt($request->password),
             'first_name' => $request->first_name,
             'second_name' => $request->second_name,
