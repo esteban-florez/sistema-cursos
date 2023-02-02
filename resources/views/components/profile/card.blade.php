56eb2545361e83083c2182872900d6c23670be28
MariYaqueno <myaqueno@gmail.com>
2023-02-02 17:16:04 -0400

    Funcional cards de cursos en perfil

--- resources/views/components/profile/card.blade.php
+++ resources/views/components/profile/card.blade.php
@@ -1,10 +1,12 @@
+@props(['image' => null, 'alt'])
+
 <div class="col-sm-6 col-md-12">
   <div class="card mx-2 profile-card-shadow">
     <div class="row no-gutters">
       <div class="col-md-4">
         <img class="img-fluid h-100 rounded-left img-cover"
-          src="{{ asset('img/html.png') }}"
-          alt="Imagen del Curso: ">
+          src="{{ $image }}"
+          alt="Imagen del {{ $alt }}: ">
       </div>
       <div class="col-md-8">
         <div class="card-body">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,5 +2,9 @@
   @push('css')
     <link rel="stylesheet" href="{{ asset('css/perfil.css') }}">
   @endpush
-  <x-profile :user="$user"></x-profile>
+  <x-profile 
+    :user="$user" 
+    :courses="$courses"
+    :clubs="$clubs"
+    />
 </x-layout.main>
