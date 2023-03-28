315987adf4d35dbc87491aa94a5a0d578f03a75f
esteban-florez <eflorez077@gmail.com>
2023-03-27 22:20:15 -0400

    fixed user-payments pagination, added loading state on schedule

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -5,12 +5,16 @@
   @push('css')
     <link rel="stylesheet" href="{{ asset('css/timetablejs.css') }}">
     <link rel="stylesheet" href="{{ asset('css/schedule.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/loading.css') }}">
   @endpush
   @push('js')
     <script defer src="{{ asset('js/timetable.js') }}"></script>
     <script defer src="{{ asset('js/schedule.js') }}"></script>
   @endpush
-  <section class="container-fluid">
+  <div class="loading-container">
+    <div class="loading"></div>
+  </div>
+  <section class="container-fluid hidden-section">
     <div class="empty-container">
       <h2 class="empty">No hay ningún evento que mostrar.</h2>
     </div>
